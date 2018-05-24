42940767b226b2f736c1e56c4503fc398b574296
bingenGalartza <bingen.galartza@alumni.mondragon.edu>
2018-05-24 16:35:42 +0200

    ADC konpondu

--- ourADC.h
+++ ourADC.h
@@ -4,8 +4,9 @@
 #include <stdint.h>
 #include <stm32f407xx.h>
 
-void initADC();
-void ourADCHandler();
-uint32_t getBalioa();
+void initADC(void);
+void ourADCHandler(void);
+uint32_t getBalioa(void);
+void setCallBack(void (*funtzioa)(uint16_t));
 
 #endif
